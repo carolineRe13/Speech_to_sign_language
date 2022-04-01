d78cf84fbf9d440203a72ac4a9c0a635e6533e1e
carolineRe13 <caroline112010@hotmail.de>
2022-04-02 00:25:06 +0200

    Save audio file

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from flask import Flask
+from flask import request
 
+app = Flask(__name__)
+
+
+@app.route("/", methods=['POST'])
+def index():
+    f = request.files['audio_data']
+    with open('audio.wav', 'wb') as audio:
+        f.save(audio)
+    print('file uploaded successfully')
+
+    return "200"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
